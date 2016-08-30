3a3ba6f9ff24b9048f564ec6bba2f22df6e71a1f
Bang Nguyen <bangnv77@gmail.com>
2016-08-29 18:34:16 -0700

    create favorite-questions service and display favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   fullQuestion: Ember.computed('question.author', 'question.date', 'question.notes', function() {
     return this.get('question.author') + ' made this question on ' + this.get('question.date') + '. He/She is interested in ' + this.get('question.notes');
-  })
+  }),
+
+  favoriteQuestions: Ember.inject.service(),
+  actions: {
+    addToFav(question) {
+      this.get('favoriteQuestions').add(question);
+    }
+  }
 });
